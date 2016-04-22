101b99fa16bf48814419e1acc3c763c28b6acb90
Михаил Логутов <logutov@gmail.com>
2016-04-22 12:37:20 +0300

    Added nuget packages scripts and config

---
+++ nuget-publish.ps1
@@ -0,0 +1,13 @@
+cls
+
+cd $PSScriptRoot
+
+$id = ((Get-Item -Path ".\..").Name)
+iex "nuget pack -build -Prop Configuration=Release -sym $id\$id.csproj"
+
+$package_file = @(Get-ChildItem "*.nupkg" -Exclude "*.symbols.*" | Sort-Object -Property CreationTime -Descending)[0]
+$package_file.Name
+
+iex ("nuget push " + $package_file.Name)
+
+Remove-Item *.nupkg
