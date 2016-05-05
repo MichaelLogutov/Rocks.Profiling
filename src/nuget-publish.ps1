7712b518e6e278c4415cc2f4f6cce916e601026a
Михаил Логутов <logutov@gmail.com>
2016-05-05 15:15:24 +0300

    nuget ps script fix for new nuget

--- src/nuget-publish.ps1
+++ src/nuget-publish.ps1
@@ -8,6 +8,6 @@ iex "nuget pack -build -Prop Configuration=Release -sym $id\$id.csproj"
 $package_file = @(Get-ChildItem "*.nupkg" -Exclude "*.symbols.*" | Sort-Object -Property CreationTime -Descending)[0]
 $package_file.Name
 
-iex ("nuget push " + $package_file.Name)
+iex ("nuget push " + $package_file.Name + " -source https://www.nuget.org/api/v2/package")
 
 Remove-Item *.nupkg
